0f6af04afb3e554987fa7051d7321d7f1e68072d
Amanda Luff <aluff@ucsd.edu>
2015-01-28 10:01:19 +0000

    Fixed Student behavior

--- static/js/hciproj.js
+++ static/js/hciproj.js
@@ -2,19 +2,28 @@
 
 $('#student').click(function(){
     var wrapper = $('#buttons'),
-        current = wrapper.children().first(),
+    	current = wrapper.children().first(),
+        back = $('<img src="images/arrow-left.png">').css({
+        	marginRight:'20%',
+        	height:25,
+        	display: 'none',
+        }),
         next = $('<form> UCSD e-mail: <input type="text" name="email"><br> PID: <input type="text" name="pid" id="pid">').css({
             height:100,
             backgroundColor: 'seashell',
             marginLeft:'100%',
             display:'none'
+        }),
+        login = $('<button id="login" class="btn">Log in</button>').css({
+        	position: 'relative'
         });
 
     
     current.fadeOut(200, function() {
         wrapper.children().next().remove();
         $(this).remove();
-        wrapper.prepend(next);
+        wrapper.prepend(back, next, login);
+        back.show().animate({marginLeft:0},200);
         next.show().animate({marginLeft:0},800);
     });
 });
