67274466f6fb00b5333a51942069fb4b4b99ede3
Amanda Luff <aluff@ucsd.edu>
2015-01-29 12:11:08 -0800

    Try to limit horizontal scroll

--- static/js/hciproj.js
+++ static/js/hciproj.js
@@ -3,29 +3,30 @@
 $('#student').click(function(){
     var wrapper = $('#buttons'),
     	current = wrapper.children().first(),
-        back = $('<img src="images/arrow-left.png">').css({
-        	marginRight:'20%',
+        back = $('<img src="images/arrow-left.png" class="back">').css({
+            marginLeft: '100%',
         	height:25,
         	display: 'none',
         }),
-        next = $('<form> UCSD e-mail: <input type="text" name="email"><br> PID: <input type="text" name="pid" id="pid">').css({
+        next = $('<form> UCSD e-mail: <input type="text" name="email"><br><br> PID: <input type="text" name="pid" id="pid">').css({
             height:100,
             backgroundColor: 'seashell',
-            marginLeft:'100%',
             display:'none'
         }),
         login = $('<button id="login" class="btn">Log in</button>').css({
         	position: 'relative',
+            marginLeft: '100%',
             display: 'none'
         });
 
     
-    current.fadeOut(200, function() {
+    current.fadeOut(300, function() {
         wrapper.children().next().remove();
         $(this).remove();
         wrapper.prepend(back, next, login);
         back.show().animate({marginLeft:0},200);
-        next.show().animate({marginLeft:0},800);
+        next.show().animate({marginLeft:0}, 300);
+
         login.show().animate({marginLeft:0},800);
     });
 });
@@ -33,12 +34,12 @@ $('#student').click(function(){
 $('#prof').click(function(){
     var wrapper = $('#buttons'),
         current = wrapper.children().first(),
-        back = $('<img src="images/arrow-left.png">').css({
+        back = $('<img src="images/arrow-left.png" class="back">').css({
         	marginRight:'20%',
         	height:25,
         	display: 'none',
         }),
-        next = $('<form> UCSD e-mail: <input type="text" name="email"><br> PID: <input type="text" name="pid" id="pid">').css({
+        next = $('<form> UCSD e-mail: <input type="text" name="email"><br><br><br> PID: <input type="text" name="pid" id="pid">').css({
             height:100,
             backgroundColor: 'seashell',
             marginLeft:'100%',
@@ -58,6 +59,7 @@ $('#prof').click(function(){
         login.show().animate({marginLeft:0},800);
     });
 });
+
 
 	
 
