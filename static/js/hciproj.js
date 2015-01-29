9b1d2975bf9b46d95ab658155f94eb78def19449
Amanda Luff <aluff@ucsd.edu>
2015-01-29 20:20:25 +0000

    Fixed button slide in

--- static/js/hciproj.js
+++ static/js/hciproj.js
@@ -11,6 +11,7 @@ $('#student').click(function(){
         next = $('<form> UCSD e-mail: <input type="text" name="email"><br><br> PID: <input type="text" name="pid" id="pid">').css({
             height:100,
             backgroundColor: 'seashell',
+            marginLeft: '100%',
             display:'none'
         }),
         login = $('<button id="login" class="btn">Log in</button>').css({
@@ -24,9 +25,8 @@ $('#student').click(function(){
         wrapper.children().next().remove();
         $(this).remove();
         wrapper.prepend(back, next, login);
-        back.show().animate({marginLeft:0},200);
-        next.show().animate({marginLeft:0}, 300);
-
+        back.show().animate({marginLeft:0},800);
+        next.show().animate({marginLeft:0}, 800);
         login.show().animate({marginLeft:0},800);
     });
 });
@@ -35,18 +35,19 @@ $('#prof').click(function(){
     var wrapper = $('#buttons'),
         current = wrapper.children().first(),
         back = $('<img src="images/arrow-left.png" class="back">').css({
-        	marginRight:'20%',
+        	marginLeft:'100%',
         	height:25,
         	display: 'none',
         }),
-        next = $('<form> UCSD e-mail: <input type="text" name="email"><br><br><br> PID: <input type="text" name="pid" id="pid">').css({
+        next = $('<form> UCSD e-mail: <input type="text" name="email"><br><br> PID: <input type="text" name="pid" id="pid">').css({
             height:100,
             backgroundColor: 'seashell',
-            marginLeft:'100%',
+            marginLeft: '100%',
             display:'none'
         }),
         login = $('<button id="login" class="btn">Log in</button>').css({
         	position: 'relative',
+            marginLeft: '100%',
             display: 'none'
         });
 
@@ -54,7 +55,7 @@ $('#prof').click(function(){
         wrapper.children().next().remove();
         $(this).remove();
         wrapper.prepend(back, next, login);
-        back.show().animate({marginLeft:0},200);
+        back.show().animate({marginLeft:0},800);
         next.show().animate({marginLeft:0},800);
         login.show().animate({marginLeft:0},800);
     });
