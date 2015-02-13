76daff0c3283e85159d297def204821b1cd0153c
Amanda Luff <aluff@ucsd.edu>
2015-02-13 03:50:18 +0000

    Added calendar/working on public appointments

---
+++ public/js/viewschedule.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	$('#calendar').fullCalendar({
+		allDaySlot: false,
+		businessHours: {
+			start: '12:00',
+			end: '14:00',
+			dow: [4]
+		},
+		defaultView: 'agendaWeek',
+		fixedWeekCount: false,
+		header: {
+			left: 'today',
+			center: 'title',
+			right: 'prev,next'
+		},
+		minTime: "08:00:00",
+		maxTime: "20:00:00",
+		scrollTime: '11:00:00',
+		weekends: false,
+		events: [
+		{
+			title: "Scott's Office Hours",
+			start:'2015-02-12T12:00:00',
+			end:'2015-02-12T14:15:00'
+		}
+		],
+
+	    eventClick: function(calEvent, jsEvent, view) {
+			window.location.href='/student_make_appt';
+	    }
+	})
+});
+
+
